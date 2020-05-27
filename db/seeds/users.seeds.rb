50c45277e2ab89e5f32671b7d34fbba270a01fa8
rkyankya <rkyankya@gmail.com>
2020-05-28 00:06:04 +0300

    Storage yml file update

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,7 +1,7 @@
 after 'schools' do
   school = School.first
 
-  puts 'Seeding users (production, idempotent)'
+  # puts 'Seeding users (production, idempotent)'
 
   school.users.where(email: 'admin@acu.com').first_or_create!(name: 'Admin User', title: 'Super Admin', password: 'foobar')
   school.users.where(email: 'student1@acu.com').create!(name: 'First Student ', title: 'Super Admin', password: 'foobar')
