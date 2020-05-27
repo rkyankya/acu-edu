07f87ab2d9fe144b8a839a6c962d01b1596ace63
rkyankya <rkyankya@gmail.com>
2020-05-27 19:22:12 +0300

    Config Production file Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
+  config.require_master_key = true
+
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
